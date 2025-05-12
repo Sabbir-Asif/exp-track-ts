c9477ea4cd80af3566f7098e98d7c0a910b9190e
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2025-05-12 09:46:45 -0700

    Revert "converted to typescript"
    This reverts commit 2a7f0b0d5e9b2ecfb7e2edaa1ee378230b749c97.

--- dist/categories.js
+++
@@ -1,2 +0,0 @@
-export const incomeCategories = ["Salary", "Freelance", "Gift", "Other"];
-export const expenseCategories = ["Food", "Transport", "Entertainment", "Bills", "Other"];
--- src/categories.ts
+++
@@ -1,2 +0,0 @@
-export const incomeCategories : string[] = ["Salary", "Freelance", "Gift", "Other"];
-export const expenseCategories : string[] = ["Food", "Transport", "Entertainment", "Bills", "Other"];
